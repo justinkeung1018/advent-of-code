1aefd098f662257b73842a14af26c21c91648a89
justinkeung <justinkeung1018@gmail.com>
2022-12-06 10:41:34 +0000

    Add Day 6 solutions in Haskell

---
+++ 2022/Day 6/TuningTrouble.hs
@@ -0,0 +1,33 @@
+import Data.List
+
+main = do 
+        contents <- readFile "./input.txt"
+        print (firstPacket contents)
+        print (firstMessage contents)
+
+
+firstPacket :: String -> Int
+firstPacket ""
+  = -1
+firstPacket s@(c : cs)
+  | (length . nub . take 4) s == 4 = 4
+  | otherwise                      = 1 + firstPacket cs
+
+firstMessage :: String -> Int
+firstMessage ""
+  = -1
+firstMessage s@(c : cs)
+  | (length . nub . take 14) s == 14 = 14
+  | otherwise                        = 1 + firstMessage cs
+
+splitBy :: String -> String -> [String]
+splitBy dl s
+  = splitBy' s ""
+  where
+    splitBy' "" s'
+      = [s']
+    splitBy' s@(c : cs) s'
+      | pre == dl = s' : splitBy' suf ""
+      | otherwise = splitBy' cs (s' ++ [c])
+      where
+        (pre, suf) = splitAt (length dl) s
