810052040158a85da21c88d459c6461585bf001d
justinkeung <justinkeung1018@gmail.com>
2022-12-18 15:11:41 +0000

    Day 18 not working

---
+++ 2022/Day 18/src/BoilingBoulders.kt
@@ -0,0 +1,76 @@
+import java.io.File
+
+fun totalSurfaceArea(cubes: Set<Triple<Int, Int, Int>>): Int {
+    var connected = 0
+    for (cube in cubes) {
+        val x = cube.first
+        val y = cube.second
+        val z = cube.third
+        val adjacent = setOf(
+            Triple(x - 1, y, z),
+            Triple(x + 1, y, z),
+            Triple(x, y - 1, z),
+            Triple(x, y + 1, z),
+            Triple(x, y, z - 1),
+            Triple(x, y, z + 1)
+        )
+        connected += adjacent.filter { cubes.contains(it) }.size
+    }
+    return 6 * cubes.size - connected
+}
+
+fun exteriorSurfaceArea(cubes: Set<Triple<Int, Int, Int>>): Int {
+    val min = Triple(cubes.minOf { it.first } - 1, cubes.minOf { it.second } - 1, cubes.minOf { it.third } - 1)
+    val max = Triple(cubes.maxOf { it.first } + 1, cubes.maxOf { it.second } + 1, cubes.maxOf { it.third } + 1)
+    val water = mutableSetOf<Triple<Int, Int, Int>>()
+    val blocked = cubes.toMutableSet()
+    fun dfs(cube: Triple<Int, Int, Int>) {
+        if (blocked.contains(cube)) {
+            return
+        }
+        val x = cube.first
+        val y = cube.second
+        val z = cube.third
+        val adjacent = setOf(
+            Triple(x - 1, y, z),
+            Triple(x + 1, y, z),
+            Triple(x, y - 1, z),
+            Triple(x, y + 1, z),
+            Triple(x, y, z - 1),
+            Triple(x, y, z + 1)
+        ).filter {
+            it.first in min.first..max.first
+                    && it.second in min.second..max.second
+                    && it.third in min.third..max.third
+        }
+        blocked.add(cube)
+        water.add(cube)
+        adjacent.forEach { dfs(it) }
+    }
+
+    dfs(min)
+
+    var exteriorSurfaceArea = 0
+    for (cube in cubes) {
+        val x = cube.first
+        val y = cube.second
+        val z = cube.third
+        val adjacent = setOf(
+            Triple(x - 1, y, z),
+            Triple(x + 1, y, z),
+            Triple(x, y - 1, z),
+            Triple(x, y + 1, z),
+            Triple(x, y, z - 1),
+            Triple(x, y, z + 1)
+        )
+        exteriorSurfaceArea += adjacent.filter { water.contains(it) }.size
+    }
+    return exteriorSurfaceArea
+}
+
+fun main() {
+    val input = File("./input.txt").readLines()
+    val cubes = input.map { it.split(",") }.map { Triple(it[0].toInt(), it[1].toInt(), it[2].toInt()) }.toMutableSet()
+    println("Part 1: ${totalSurfaceArea(cubes)}")
+    println("Part 2: ${exteriorSurfaceArea(cubes)}")
+}
